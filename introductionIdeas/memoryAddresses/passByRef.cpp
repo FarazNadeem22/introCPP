63586f40bab4210b8fb572d7b33997b0a7b060e0
Faraz Nadeem <muhammad.ali.faraz@gmail.com>
2023-07-19 14:58:32 -0400

    pass by ref working

---
+++ introductionIdeas/memoryAddresses/passByRef.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+// using & in swap function to pass by reference
+
+using std::cin, std::cout, std::string;
+
+void sleepSeconds(int seconds)
+{
+    // cout << "Sleeping for " << seconds << " seconds\n";
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+
+void clear()
+{
+    system("cls");
+}
+
+void printXY(string x, string y)
+{
+    cout << "X: " << x << std::endl;
+    cout << "Y: " << y << std::endl;
+}
+
+void swap(string &x, string &y)
+{
+    string temp = x;
+    x = y;
+    y = temp;
+}
+
+int main()
+{
+
+    clear();
+    string x = "Water";
+    string y = "Ice";
+    string temp;
+
+    printXY(x, y);
+    cout << "*********************\n";
+
+    swap(x, y);
+    printXY(x, y);
+
+    return 0;
+}
