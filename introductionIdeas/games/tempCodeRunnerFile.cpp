55b09372577752d3103e82e99c3551ba212e8826
Faraz Nadeem <muhammad.ali.faraz@gmail.com>
2023-07-21 11:14:35 -0400

    Check winner function completed and tested

--- introductionIdeas/games/tempCodeRunnerFile.cpp
+++ introductionIdeas/games/tempCodeRunnerFile.cpp
@@ -1,1 +1,2 @@
-   sleepSeconds(1);
+
+{
--- introductionIdeas/games/tictactoe.cpp
+++ introductionIdeas/games/tictactoe.cpp
@@ -175,6 +175,7 @@ void computerMove(char *spaces, char computer)
 
 bool checkWinner(char *spaces, char player, char computer)
 {
+    // Check Across
     if (spaces[0] == spaces[1] && spaces[1] == spaces[2] && spaces[0] != ' ')
     {
         spaces[0] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
@@ -183,6 +184,40 @@ bool checkWinner(char *spaces, char player, char computer)
     else if (spaces[3] == spaces[4] && spaces[4] == spaces[5] && spaces[3] != ' ')
     {
         spaces[3] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
+        return 1;
+    }
+    else if (spaces[6] == spaces[7] && spaces[7] == spaces[8] && spaces[6] != ' ')
+    {
+        spaces[6] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
+        return 1;
+    }
+
+    // Check up-down
+    if (spaces[0] == spaces[3] && spaces[3] == spaces[6] && spaces[0] != ' ')
+    {
+        spaces[0] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
+        return 1;
+    }
+    else if (spaces[1] == spaces[4] && spaces[4] == spaces[7] && spaces[1] != ' ')
+    {
+        spaces[1] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
+        return 1;
+    }
+    else if (spaces[2] == spaces[5] && spaces[5] == spaces[8] && spaces[2] != ' ')
+    {
+        spaces[2] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
+        return 1;
+    }
+
+    // Check crosses
+    if (spaces[0] == spaces[4] && spaces[4] == spaces[8] && spaces[0] != ' ')
+    {
+        spaces[0] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
+        return 1;
+    }
+    else if (spaces[6] == spaces[4] && spaces[4] == spaces[2] && spaces[2] != ' ')
+    {
+        spaces[6] == player ? cout << "YOU WIN!" : cout << "COMPUTER WINs!";
         return 1;
     }
 
