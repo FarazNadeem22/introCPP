a33b9d50b708a9535ceb4acd7b17350f91f7d2d0
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2023-12-26 04:25:57 -0500

    Update ArrToFunc.cpp
    comments added

--- introductionIdeas/ArrIntro/ArrToFunc.cpp
+++ introductionIdeas/ArrIntro/ArrToFunc.cpp
@@ -3,27 +3,41 @@
 
 using std::cin, std::cout, std::string, std::getline, std::ws, std::stoi;
 
+// Function to calculate the total sum of prices in an array.
+// Parameters:
+//   prices: An array containing prices of items.
+//   size: The number of items/prices in the array.
+// Returns:
+//   The total sum of all prices in the array.
 double getTotal(double prices[], int size);
 
 int main()
 {
+    // Array containing prices of items
     double prices[] = {29.99,
                        34.99,
                        19.99,
                        9.99,
                        7.99,
                        299.99};
+
+    // Determine the number of items/prices in the array
     int size = sizeof(prices) / sizeof(prices[0]);
 
+    // Calculate the total sum of prices
     double total = getTotal(prices, size);
 
+    // Display the total sum of prices
     cout << "Total: $" << total;
     return 0;
 }
 
+// Function to calculate the total sum of prices in an array.
 double getTotal(double prices[], int size)
 {
     double total = 0;
+
+    // Iterate through the prices array and calculate the total sum
     for (int i = 0; i < size; i++)
     {
         total += prices[i];
